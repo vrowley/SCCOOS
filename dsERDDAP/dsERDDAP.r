f1cccb04b404aea761e357b0057501baa7ef556a
Vicky Rowley <vrowley@ucsd.edu>
2018-09-17 20:29:42 -0700

    modified to add header row and only output temperature, lat, long and depth.

--- dsERDDAP/dsERDDAP.r
+++ dsERDDAP/dsERDDAP.r
@@ -1,14 +1,17 @@
+library(stringr)
+
 stations <- c("scripps_pier", "newport_pier", "stearns_wharf", "santa_monica_pier")
 
 for (station in stations){
-  get_all_years_url = str_c('http://sccoos.org/erddap/tabledap/autoss.csv?station,time,temperature,temperature_flagPrimary,temperature_flagSecondary,conductivity,conductivity_flagPrimary,conductivity_flagSecondary,pressure,pressure_flagPrimary,pressure_flagSecondary,chlorophyll,chlorophyll_flagPrimary,chlorophyll_flagSecondary,salinity,salinity_flagPrimary,salinity_flagSecondary,sigmat,diagnosticVoltage,currentDraw,aux1,aux3,aux4,latitude,longitude,depth,O2thermistor,convertedOxygen&station="',station,'"&time>=2005-06-16T19%3A27%3A53Z&time<2018-09-17T17%3A30%3A45Z')
+#  get_all_years_url = str_c('http://sccoos.org/erddap/tabledap/autoss.csv?station,time,temperature,temperature_flagPrimary,temperature_flagSecondary,conductivity,conductivity_flagPrimary,conductivity_flagSecondary,pressure,pressure_flagPrimary,pressure_flagSecondary,chlorophyll,chlorophyll_flagPrimary,chlorophyll_flagSecondary,salinity,salinity_flagPrimary,salinity_flagSecondary,sigmat,diagnosticVoltage,currentDraw,aux1,aux3,aux4,latitude,longitude,depth,O2thermistor,convertedOxygen&station="',station,'"&time>=2005-06-16T19%3A27%3A53Z&time<2018-09-17T17%3A30%3A45Z')
+  get_all_years_url = str_c('http://sccoos.org/erddap/tabledap/autoss.csv?station,time,temperature,temperature_flagPrimary,temperature_flagSecondary,latitude,longitude,depth&station="',station,'"&time>=2005-06-16T19%3A27%3A53Z&time<2018-09-17T17%3A30%3A45Z')
   file.remove("temp.csv")
   file.create("temp.csv")
   all_dest_file = str_c('autoss_',station,'_all.csv', sep='')
   file.create(all_dest_file)
   curl::curl_download(get_all_years_url, "temp.csv")
 #  write.csv(1:1000, file=temp_dest_file)
-  file.pipe <- pipe("awk 'BEGIN{i=0}{i++;if (i%4==0) print $1}' < temp.csv ")
+  file.pipe <- pipe("awk 'BEGIN{i=14}{i++;if (i%15==0) print $1}' < temp.csv ")
   res <- read.csv(file.pipe)
   write.csv(res, file = all_dest_file)
 }
